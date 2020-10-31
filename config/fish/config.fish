90518b0055171e629e2dbe758d58d060bb215769
isabella <isabellabdoamaral@usp.br>
2020-10-31 18:41:50 -0300

    updated configs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,24 +1,4 @@
 #source ~/.fancy-bash-promt.sh
-alias pac=yay
-alias btrmanager='pushd $HOME/.local/share/applications/ && gtk-launch buttermanager.desktop && popd'
-#alias yay='doas pacmatic'
-#alias sudo='doas'
-
-# pacmatic needs to be run as root: https://github.com/keenerd/pacmatic/issues/35
-alias pacmatic='sudo --preserve-env=pacman_program /usr/bin/pacmatic'
-
-# Downgrade permissions as AUR helpers expect to be run as a non-root user. $UID is read-only in {ba,z}sh.
-function yay --wraps pacman_program --description 'alias yay=pacman_program'
-            pacman_program="sudo -u isa$UID /usr/bin/yay --pacman powerpill" pacmatic $argv
-end
-
-function spot --wraps spotify-tui_program --description 'little script for spotify tui'
-	if not pgrep "spotifyd";
-		spotifyd
-	end
-	
-	spt
-end
 
 alias pls=sudo
 alias gib=pac
---
+++ config/fish/functions/pac.fish
@@ -0,0 +1,9 @@
+alias pac=yay
+# pacmatic needs to be run as root: https://github.com/keenerd/pacmatic/issues/35
+alias pacmatic='sudo --preserve-env=pacman_program /usr/bin/pacmatic'
+
+# Downgrade permissions as AUR helpers expect to be run as a non-root user. $UID is read-only in {ba,z}sh.
+function yay --wraps pacman_program --description 'alias yay=pacman_program'
+            pacman_program="sudo -u $LOGNAME$UID /usr/bin/yay --pacman powerpill" pacmatic $argv
+end
+
